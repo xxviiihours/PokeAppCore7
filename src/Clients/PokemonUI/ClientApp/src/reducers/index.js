fb83d97838106b168a2a07cabcb73714d85e431d
Kevz Macalalag <mc.kvnflrs@gmail.com>
2023-01-20 20:14:06 +0800

    Added Pokemon Reducer

--- src/Clients/PokemonUI/ClientApp/src/reducers/index.js
+++ src/Clients/PokemonUI/ClientApp/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import abilityReducer from "./features/abilitySlice";
+import pokemonReducer from "./features/pokemonSlice";
 import userInterfaceReducer from "./features/userInterfaceSlice";
 
 export default combineReducers({
     ui: userInterfaceReducer,
+    pokemon: pokemonReducer,
     ability: abilityReducer
 })
